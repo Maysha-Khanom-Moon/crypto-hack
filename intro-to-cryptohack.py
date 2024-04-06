65a3e00ddd17aaaa9cd78b4494ee5658342a9e57
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-04-06 15:25:55 +0600

    hex ^ hex: string

--- intro-to-cryptohack.py
+++ intro-to-cryptohack.py
@@ -1,16 +1,29 @@
-label = "label"
-ans = ""
+# hex ^ hex to string:
+# 1. hex to bytes
+# 2. bytes(a ^ b for a, b in zip(byte1, byte2))
+# 3. then decode it
 
-for char in label:
-    # conver char to unicode integer
-    char = ord(char)
-    
-    # xor with 13
-    char = char ^ 13
-    
-    # convert to char
-    char = chr(char)
-    
-    ans += char
+key_1 = "a6c8b6733c9b22de7bc0253266a3867df55acde8635e19c73313"
+key_1 = bytes.fromhex(key_1)
 
-print(ans)
+key_2_3 = "c1545756687e7573db23aa1c3452a098b71a7fbf0fddddde5fc1"
+key_2_3 = bytes.fromhex(key_2_3)
+
+all = "04ee9855208a2cd59091d04767ae47963170d1660df7f56f5faf"
+all = bytes.fromhex(all)
+
+
+# to byte ^ byte have to use bytes()
+flag = bytes(a ^ b ^ c for a, b, c in zip(key_1, key_2_3, all))
+
+
+print(flag.decode())
+
+
+
+
+"""
+a ^ b = c
+b ^ c = a
+c ^ a = b
+"""
